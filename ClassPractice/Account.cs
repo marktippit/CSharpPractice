8f3967ad7fd001684114330d980ef44e54e67890
marktippit <marktippit@gmail.com>
2021-12-22 11:23:08 -0700

    saving first 7 chapters

---
+++ ClassPractice/Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractice
+{
+    class Account
+    {
+        public string Name { get; set; } //auto implemented property
+        private decimal balance;
+
+        //constructor sets the Name property to parameter accountName's Value
+        public Account(string accountName, decimal initialBalance)//constructor name is class name
+        {
+            Name = accountName;
+            Balance = initialBalance;
+        }
+
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+            }
+
+            private set///can be used only within the class
+            {
+                //validate that the balance is greater than 0, if not,
+                //instance variable balance keeps its prior value
+                if (value > 0.0m) //m indicates that 0.0 is a decimal literal
+                    balance = value;
+            }
+        }
+
+        public void Deposit(decimal depositAmmount)
+        {
+            if (depositAmmount > 0.0m)
+            {
+                Balance += depositAmmount;
+            }
+        }
+    }
+}
---
+++ ClassPractice/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account1 = new Account("Jane Green", 50.00m);
+            Account account2 = new Account("John Blue", -7.53m);
+
+            //display initial balance of each object
+            Console.WriteLine(
+                $"{account1.Name}'s balace: {account1.Balance:C}");
+            Console.WriteLine(
+                $"{account2.Name}'s balance is: {account2.Balance:C}");
+
+            //prompt for then read input
+            Console.Write($"\nEnter deposit ammount for {account1.Name}\n");
+            decimal depositAmount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine(
+                $"Adding {depositAmount:C} to {account1.Name}'s account balance\n");
+            account1.Deposit(depositAmount);//add to account1's balance
+
+            //display balances
+            Console.WriteLine(
+                $"{account1.Name}'s balance: {account1.Balance}");
+            Console.WriteLine(
+                $"{account2.Name}'s balance: {account2.Balance}");
+
+            //prompt for then read input
+            Console.Write($"\nEnter deposit ammount for {account2.Name}\n");
+            depositAmount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine(
+                $"Adding {depositAmount:C} to {account2.Name}'s account balance\n");
+            account2.Deposit(depositAmount);//add to account1's balance
+
+            //display balances
+            Console.WriteLine(
+                $"{account1.Name}'s balance: {account1.Balance:c}");
+            Console.WriteLine(
+                $"{account2.Name}'s balance: {account2.Balance:c}");
+        }
+    }
+}
